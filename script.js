ca1a2b61ef52cd551d6bc3a5b3d71e0c9ea258b0
alex <cronert.alex@live.fr>
2025-09-01 16:49:59 +0200

    changed fov to 85

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 scene.add(createScene());
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/(window.innerHeight), 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(85, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 1.6, 0);
 
 // Renderer
@@ -19,12 +19,13 @@ document.body.appendChild(renderer.domElement);
 
 // Controls
 // const controls = createCameraControls(camera, renderer.domElement);
-const controls = createGyroControls(camera, renderer.domElement);
+const gyroControls = createGyroControls(camera, renderer.domElement);
 
 // Animation
 function animate() {
   requestAnimationFrame(animate);
-  controls.update();
+  // controls.update();
+  gyroControls.update();
   renderer.render(scene, camera);
 }
 animate();
