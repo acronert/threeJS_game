3927cd2b443db37f8f2b9caf82e086a20c9577bd
alex <cronert.alex@live.fr>
2025-09-01 17:17:47 +0200

    added touch to walk

--- gyroControls.js
+++ gyroControls.js
@@ -38,25 +38,42 @@ export function createGyroControls(camera, domElement) {
   });
 
   
-  let touch = false;
+  const keys = { forward:false, backward:false, left:false, right:false };
   const speed = 0.05;
 
   // Touch to walk
   domElement.addEventListener("touchstart", (e) => {
-    touch = true;
+    const touch = e.touches[0];
+
+    const rect = domElement.getBoundingClientRect();  // display size
+    console.log("width:", rect.width, "height:", rect.height);
+    if (touch.clientX < rect.width / 4)   keys.left = true;
+    else if (touch.clientX > 3 * rect.width / 4)   keys.right = true;
+    else if (touch.clientY > 7 * rect.height / 8)  keys.backward = true;
+    else    keys.forward = true;
   });
 
   domElement.addEventListener("touchend", (e) => {
-    touch = false;
+    keys.forward = false;
+    keys.backward = false;
+    keys.left = false;
+    keys.right = false;
   });
 
   function update() {
     // forward direction in local space is (0, 0, -1)
-    const direction = new THREE.Vector3(0, 0, -1);
-    direction.applyQuaternion(camera.quaternion); // rotate with camera orientation
-    direction.y = 0; // ignore vertical tilt, keep movement flat on ground
-    direction.normalize();
-    if (touch)  camera.position.addScaledVector(direction, speed);
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(camera.quaternion);
+    forward.y = 0; // keep movement on ground plane
+    forward.normalize();
+
+    // right direction is perpendicular to forward & up
+    const right = new THREE.Vector3().crossVectors(forward, new THREE.Vector3(0, 1, 0)).normalize();
+
+    if (keys.forward)  camera.position.addScaledVector(forward, speed);
+    if (keys.backward)  camera.position.addScaledVector(forward, -speed);
+    if (keys.left)  camera.position.addScaledVector(right, -speed);
+    if (keys.right)  camera.position.addScaledVector(right, speed);
   }
   
   return { update }
