d1de25a973790c83ca02c30417c10c0083ca2610
alex <cronert.alex@live.fr>
2025-09-01 16:37:10 +0200

    added touch to walk

--- gyroControls.js
+++ gyroControls.js
@@ -1,48 +1,3 @@
-// // import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
-
-// // export function createGyroControls(camera, domElement) {
-
-// //   let yaw = 0, pitch = 0, roll = 0;
-// //   const speed = 0.06;
-// //   const rotation_speed = 0.03; // in radians
-
-// //     window.addEventListener("deviceorientation", (event) => {
-// //     const degToRad = Math.PI / 180;
-// //     yaw = (event.alpha ?? 0) * degToRad; // z-axis
-// //     pitch = (event.beta  ?? 0) * degToRad; // x-axis
-// //     roll = (event.gamma ?? 0) * degToRad; // y-axis
-// //     console.log("yaw: " + yaw + ", pitch: " + pitch + " , roll: " + roll);
-    
-// //   });
-  
-// //   function update() {
-// //     const euler = new THREE.Euler(pitch, yaw, -roll, 'ZYX');
-// //     camera.setRotationFromEuler(euler);
-// //     }
-
-// //     return { update }
-// // }
-
-// import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
-
-// export function createGyroControls(camera, domElement) {
-
-//   let yaw = 0, pitch = 0, roll = 0;
-//   const degToRad = Math.PI / 180;
-
-//   window.addEventListener("deviceorientation", (event) => {
-//     yaw = (event.alpha ?? 0) * degToRad; // z-axis 0 to 360 degrees     yaw
-//     pitch  = (event.beta  ?? 0) * degToRad; // x-axis -180 to 180 degrees  pitch
-//     roll = (event.gamma ?? 0) * degToRad; // y-axis -90 to 90 degrees    roll
-//   });
-
-
-//   function update() {
-//   }
-
-//   return { update }
-// }
-
 import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
 
 export function createGyroControls(camera, domElement) {
@@ -82,9 +37,27 @@ export function createGyroControls(camera, domElement) {
     camera.quaternion.copy(tempQuaternion);
   });
 
-  function update() {
-    // Pas besoin de faire quelque chose ici, la caméra est déjà mise à jour dans l'event
-  }
+  
+  let touch = false;
+  const speed = 0.05;
 
+  // Touch to walk
+  domElement.addEventListener("touchstart", (e) => {
+    touch = true;
+  });
+
+  domElement.addEventListener("touchend", (e) => {
+    touch = false;
+  });
+
+  function update() {
+    // forward direction in local space is (0, 0, -1)
+    const direction = new THREE.Vector3(0, 0, -1);
+    direction.applyQuaternion(camera.quaternion); // rotate with camera orientation
+    direction.y = 0; // ignore vertical tilt, keep movement flat on ground
+    direction.normalize();
+    if (touch)  camera.position.addScaledVector(direction, speed);
+  }
+  
   return { update }
 }
